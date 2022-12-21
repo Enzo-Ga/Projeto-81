22904025e01588dd4de8f576361c0e26576adc82
Enzo-Ga <93016718+Enzo-Ga@users.noreply.github.com>
2022-12-20 21:31:39 -0300

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text, StyleSheet } from "react-native";
+
+export default class Feed extends Component {
+  render() {
+    return <Text>Feed</Text>;
+  }
+}
